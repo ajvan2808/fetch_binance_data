272ff76d4108f857b23c2deab0cd4dc7df42f65f
Hanh Van <58590451+ajvan2808@users.noreply.github.com>
2022-01-20 13:51:45 +0700

    Delete jobs_scheduler.py

--- jobs_scheduler.py
+++
@@ -1,9 +0,0 @@
-from crontab import CronTab
-
-with CronTab(user='root') as cron:
-    job = cron.new(command='fetch_data.py')
-
-    for job in cron:
-        job.minute.every(1)
-        cron.write('output.tab')
-        print(job)
